450a4296d7c470fc62cc44d099b76c29200c0542
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2022-04-21 18:10:45 -0700

    Created Boiler Plate for Project 3
    Created the files for Models, Automobiles, Trims, Features, AvailablePackages, and Packages.

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Scanner;
+
+import jakarta.persistence.*;
+import model.*;
+public class App {
+    private static void test(){
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        test();
+    }
+}
