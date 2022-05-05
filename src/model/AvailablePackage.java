d494b944c31ce4225ea217f7e39f130eb74cc3ba
MNRainDrop <79126834+MNRainDrop@users.noreply.github.com>
2022-05-05 08:26:11 -0700

    Updated AvailblePackage class

--- src/model/AvailablePackage.java
+++ src/model/AvailablePackage.java
@@ -1,9 +1,9 @@
 package model;
 
 public class AvailablePackage {
-    private static float cost;
+    private float cost;
 
-    public static float getCost() {
+    public float getCost() {
         return cost;
     }
 
