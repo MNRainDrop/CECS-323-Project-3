4304cf52352c6bab3cf9a4ca194d52c81fb5384f
Christine Bui <Christine.Bui@student.csulb.edu>
2022-05-07 15:43:33 -0700

    Created ChosenPackages

---
+++ src/model/ChosenPackages.java
@@ -0,0 +1,33 @@
+package model;
+import jakarta.persistence.*;
+
+@Entity(name="chosenpackages")
+public class ChosenPackages {
+    //Foreign key with availablepackage
+    //Many to One, Unidirectional?
+    @ManyToOne
+    @JoinColumn(name = "available_id" )
+    private AvailablePackage availablepackage;
+
+    public AvailablePackage getAvailablepackage() {
+        return availablepackage;
+    }
+
+    public void setAvailablepackage(AvailablePackage availablepackage) {
+        this.availablepackage = availablepackage;
+    }
+
+    //Foreign key with automobiles
+    //Many to One, Unidirectional?
+    @ManyToOne
+    @JoinColumn(name = "auto_id" )
+    private Automobile automobile;
+
+    public Automobile getAutomobile() {
+        return automobile;
+    }
+
+    public void setAutomobile(Automobile automobile) {
+        this.automobile = automobile;
+    }
+}
